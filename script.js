d033696315b5cc4779064efb25b7ff55bbe9c7d0
Aayushmakc <rojikc764@gmail.com>
2024-10-08 09:32:37 +0545

    Initial commit: Add portfolio website files

---
+++ script.js
@@ -0,0 +1,20 @@
+(function(){
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        });
+    });
+    document.querySelector(".theme-btn").addEventListener("click",()=>{
+        document.body.classList.toggle("light-mode");
+    })
+})();
+var type = new Typed(".text",{
+    strings:["Web developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
